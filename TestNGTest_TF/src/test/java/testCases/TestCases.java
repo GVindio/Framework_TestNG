63107cede55b9dbd55d90e05c0ec2767ba7e01eb
GVindio <97354117+GVindio@users.noreply.github.com>
2022-03-28 19:53:47 -0400

    First Commit - TestNG Framework

---
+++ TestNGTest_TF/src/test/java/testCases/TestCases.java
@@ -0,0 +1,72 @@
+package testCases;
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import testBase.TestBase;
+import testPages.TestPages;
+
+public class TestCases extends TestBase {
+	
+	TestPages testpages;
+
+	@BeforeTest
+	public void setUp() {
+
+		startBrowser(); // Arrived at page
+		testpages = new TestPages();
+
+}
+	
+	@Test
+	public void validateCategory() {
+		/*1. Arrived at page 
+		2. Add a category
+		3. Click on add category
+		4. Verify category displays
+		Result: When a category is added The category is displayed*/
+		
+		int min = 0;
+		int max = 10000000;
+		int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
+		String random = String.valueOf(random_int);
+		
+		testpages.addCategory(random); // Adding a Category
+		boolean verify = testpages.verifyHelloMoon(random); // Verifying Category - Returns a true or false value
+		Assert.assertTrue(verify);	// Expect the Category is displayed so we expect True
+
+	}
+	
+	@Test
+	public void validateDuplicatedCategory() {
+	/*  1. Arrived at page
+		2. Add a duplicate Category
+		3. Duplicate Category msg should display 
+		Result: When a duplicate Category is added msg displays 
+		"The category you want to add already exist:
+		   <dupicated category name>"  */
+		testpages.addDupCategory(); // Adding Duplicate Category
+		//testpages.addCatBtn(); // Click on Add Category button
+		
+}
+	@Test
+	public void validatemonths() {
+		/*1. Arrived at page
+		2. Click on months button
+		3. Validate all the months are in the months drop down section
+		Results: All the months are displayed on drop down section */
+		//testpages.verifyMonths();
+		//Assert.assertTrue(testpages.verifyMonths());
+		boolean x = testpages.verifyMonths();
+		Assert.assertTrue(x);
+	}
+	
+	@AfterTest
+	public void breakDown() {
+		 //driver.quit();
+	}
+}
---
+++ TestNGTest_TF/src/test/java/testPages/TestPages.java
@@ -0,0 +1,80 @@
+package testPages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import testBase.TestBase;
+
+public class TestPages extends TestBase {
+
+	@FindBy(name = "categorydata")
+	WebElement inputCategory;
+
+	@FindBy(xpath = "//input[@value='Add category']")
+	WebElement addCatBtn;
+
+	@FindBy(name = "due_month")
+	WebElement verifyMonths;
+
+	public TestPages() {
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void addCategory(String random) {
+		inputCategory.sendKeys(random);
+		addCatBtn.click();
+
+	}
+
+	public boolean verifyHelloMoon(String random) {
+		WebElement category = driver.findElement(By.xpath("//span[contains(text(),'" + random + "')]"));
+
+		return category.isDisplayed();
+
+		/*
+		 * if(driver.getPageSource().contains("Hello Moon!"))
+		 * System.out.println("Category is displayed"); else {
+		 * System.out.println("Category is not displayed"); }
+		 */
+
+	}
+
+	public void addDupCategory() {
+		inputCategory.sendKeys("Hello Moon!");
+
+		if (driver.getPageSource().contains("Hello Moon"))
+			System.out.println("Category already exists:");
+		else {
+			System.out.println("Category does not exist:");
+		}
+
+	}
+
+	public boolean verifyMonths() {
+		verifyMonths.click();
+		
+	      Select s = new Select(driver.findElement(By.name("due_month"))); // Select helps you to interact with a drop menu and your passing the webelement that is a dropdown
+	      // getting the list of options in the dropdown with getOptions()
+	      List <WebElement> op = s.getOptions(); // List being an empty container and s being a drop down with be filled on whats inside the element.
+	      int size = op.size();  //save count into the integer
+	      
+	      for(int i =0; i<size ; i++){
+	         String options = op.get(i).getText();
+	         System.out.println(options);
+	         
+	      }
+		if(size==13) { // Expecting  12 months + the none option.
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+}
